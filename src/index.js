f4c68e836c74265bd20d81aeb8895b448c45747d
Steve Bright <swemhtet.net@gmail.com>
2024-04-04 18:32:39 +0630

    backup emergency blackout

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { getDatabase, ref, onValue } from "firebase/database";
 
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 // import { getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+//in case npm does not work. 
 
 const firebaseConfig = {
     databaseURL: "https://personal-budget-tracker-63862-default-rtdb.asia-southeast1.firebasedatabase.app/"
